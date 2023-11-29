86cbff3e102cc41d5a756027330e39e2eefa268a
malikadil45 <justwait2mint@gmail.com>
2023-11-30 04:55:11 +0500

    addneworder

--- app/Views/addneworder.php
+++ app/Views/addneworder.php
@@ -6,7 +6,7 @@
 
     <?php include('sidebar.php'); ?>
 
-    <!-- Content Wrapper. Contains page content -->
+    <!-- Content Wrapper. Contains page content adil-->
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <section class="content-header">
