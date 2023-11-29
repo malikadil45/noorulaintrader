9e6291c480b823fd033eb17847f6e582eb68629e
malikadil45 <justwait2mint@gmail.com>
2023-11-30 04:37:00 +0500

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'https://malikadil45.github.io/noorulaintrader/';
+    public string $baseURL = 'https://malikadil45.github.io/noorulaintrader/public';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
